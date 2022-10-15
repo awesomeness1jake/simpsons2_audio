c57dcf1602e4d05da3e8ceb4417a86348dd37567
awesomeness1jake <69776285+awesomeness1jake@users.noreply.github.com>
2022-10-14 17:36:16 -0700

    Start

---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'bodacious'
+game 'gta5'
+
+files { 
+	"audio/sfx/dlc_sp_sounds/marge_simpson.awc",
+	"audio/sfx/dlc_sp_sounds/homer_simpson.awc",
+	'audio/sfx/dlc_sounds/collect_soundfx.awc',
+	'audio/sfx/dlc_sounds/simpsons2_gameplay.awc',
+	
+	'audio/sfx/dlc_sounds/simpsons2_sunday_drive_end_sus1.awc',
+	'audio/sfx/dlc_sounds/simpsons2_sunday_drive_end_sus2.awc',
+	'audio/sfx/dlc_sounds/simpsons2_sunday_drive_end_sus3.awc',
+	'audio/sfx/dlc_sounds/simpsons2_sunday_drive.awc',
+	
+	'audio/sfx/dlc_sounds/hit_run_main.awc',
+	'audio/sfx/dlc_sounds/hit_run_end_neg.awc',
+	'audio/sfx/dlc_sounds/hit_run_intro.awc',
+	
+	"audio/config/simpsons2_game.dat151.rel",
+	"audio/config/simpsons2_speech.dat4.rel",
+	"audio/config/simpsons2_sounds.dat54.rel",
+}
+
+data_file "AUDIO_GAMEDATA" 'audio/config/simpsons2_game.dat'
+data_file "AUDIO_SOUNDDATA" "audio/config/simpsons2_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/sfx/dlc_sounds"
+data_file "AUDIO_WAVEPACK" "audio/sfx/dlc_sp_sounds"
+data_file "AUDIO_SPEECHDATA" "audio/config/simpsons2_speech.dat"
+
+
